6c61ab6b75f372d4a50a2a288c9d2eb92e357117
Alina Pisarenko <pisarenko.alina@gmail.com>
2022-11-04 14:20:15 -0400

    initial code

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,22 +1,11 @@
 import React from 'react'
 
-export default function CharacterCard({character, handleDelete, handleUpdate}) {
-
-
-//destructuring an object, so we can use it's key-value pairs without typing character.name, character.img etc
-const {id, name, image, isGood} = character
+export default function CharacterCard() {
 
   return (
     <div className="CharacterCard">
-      <h3>{name}</h3>
-      <img src={image} alt='character'/>
-      {/* 
-      Calling functions onClick
-      This functions passed from a parent component as props. 
-      Passing an id of an object as argument to have reference to a specific character
-       */}
-      <button onClick={() => handleUpdate(id)} >{isGood ? "I'm good 😇" : "I'm bad 😈"}</button>
-        <button onClick={() => handleDelete(id)} className="remove-btn">REMOVE</button>
+      <button>Replace me 👀</button>
+        <button className="remove-btn">REMOVE</button>
     </div>
   )
 }
--- src/components/CharactersList.js
+++ src/components/CharactersList.js
@@ -1,64 +1,16 @@
 import {useState} from 'react'
-//importing data from our fake DB
 import { characters, getNewRandomCharacter } from "../data";
 import CharacterCard from './CharacterCard';
 
 
 export default function CharactersList() {
-//setting initial state to the array from our fake DB
 const [allCharacters, setAllCharacters] = useState(characters)
 
 
-//function that runs onClick to add a new character
-function handleAddCharacter() {
-    const newCharacter = getNewRandomCharacter()
-//using spread operator to update te state with a copy of allCharacters array and add a new object to that copy
-    setAllCharacters([...allCharacters, newCharacter])
-}
-
-//function that runs onClick to remove a character
-function handleDelete(id) {
-
-// creating a new array using .filter(), 
-// Only returning elements where id is not matching the id that we passed as a parameter
-const updatedArray = allCharacters.filter(character => {
-    return character.id !== id
-})
-//updating state witt the updatedArray
-setAllCharacters(updatedArray)
-}
-
-
-//function that runs onClick to update an object in the array(changing isGood to the opposite  value)
-function handleUpdate(id) {
-
-//creating a new array using .map/
-//finding an object, where id is matching to the one that we passed as argument
-const newArray = allCharacters.map(character => {
-    if (character.id === id) {
-        //if found the match -> update that object with new info
-       return {...character, isGood: !character.isGood}
-    } else {
-        //if didn't find the match return unchanged object
-        return character
-    }
-})
-//set state to the newArray
-setAllCharacters(newArray)
-
-}
-
   return (
-    <div>
-    <button onClick={handleAddCharacter}>Add new character</button>
     <div className='CharactersList'>
-        {/* 
-        Calling .map() on alCharacters array and returning a new CharacterCard component for each object in that array. 
-        Passing the whole character object and 2 functions as props
-        */}
         {allCharacters.map(character => {
-        return <CharacterCard key={character.id} character={character} handleDelete={handleDelete} handleUpdate={handleUpdate} />})}
-    </div>
+        return <CharacterCard key={character.id} />})}
     </div>
   )
 }
